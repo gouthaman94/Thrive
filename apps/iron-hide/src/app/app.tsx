4535bd75b7f90c2ca4f685c3cb65328f243b83d9
gouthaman <gouthaman.ganesh@gmail.com>
2021-06-21 01:27:33 +0530

    test husky 2

--- apps/iron-hide/src/app/app.tsx
+++ apps/iron-hide/src/app/app.tsx
@@ -1,7 +1,7 @@
 import styles from "./app.module.scss";
 import { TopBar, SideBar } from "@iron-hide/nav-bar";
 
-//test hook
+//test hook 2
 export function App() {
   return (
     <div className={styles.app}>
