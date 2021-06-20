d4ac906f875aca0839fb64cd19dca6b3dafa46d2
gouthaman <gouthaman.ganesh@gmail.com>
2021-06-21 01:14:30 +0530

    Remove unused interfaces

--- libs/nav-bar/src/lib/side-bar/side-bar.tsx
+++ libs/nav-bar/src/lib/side-bar/side-bar.tsx
@@ -1,8 +1,5 @@
 import styles from "./side-bar.module.scss";
 
-/* eslint-disable-next-line */
-export interface SideBarProps {}
-
 export function SideBar() {
   return <div className={styles.side_bar}></div>;
 }
--- libs/nav-bar/src/lib/top-bar/top-bar.tsx
+++ libs/nav-bar/src/lib/top-bar/top-bar.tsx
@@ -1,10 +1,7 @@
 import Styles from "./top-bar.module.scss";
 import logo from "./__assets__/thrive-logo.png";
 
-/* eslint-disable-next-line */
-export interface ITopBarProps {}
-
-export function TopBar(props: ITopBarProps) {
+export function TopBar() {
   return (
     <div className={Styles.top_bar}>
       <div className={Styles.top_bar__left_items}>
