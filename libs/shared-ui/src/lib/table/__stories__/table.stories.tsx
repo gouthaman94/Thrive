35785b030cc54f8322a7a004f83975be62b085b4
janoh1993 <janoh@thriveconsole.com>
2021-07-12 09:57:55 +0530

    Testing commit

--- libs/shared-ui/src/lib/table/__stories__/table.stories.tsx
+++ libs/shared-ui/src/lib/table/__stories__/table.stories.tsx
@@ -2,6 +2,7 @@ import { storiesOf } from "@storybook/react";
 import { Table } from "../table";
 import { makeData } from "../__mocks__/table-mock-data";
 import { action } from "@storybook/addon-actions";
+//import { Dropdown } from "../table-dropdown/dropdown";
 
 const { columns, data } = makeData(20);
 storiesOf("1 - Components/Table", module).add("Default", () => {
@@ -15,3 +16,28 @@ storiesOf("1 - Components/Table", module).add("Default", () => {
     />
   );
 });
+
+// storiesOf("1 - Components/Table-2", module).add("Default", () => {
+//   return (
+//     <Table
+//       columns={columns}
+//       data={data}
+//       sortData={action}
+//       currentSort={{}}
+//       onCellRangeSelection={action}
+//     />
+//   );
+// });
+
+// storiesOf("1 - Components/Table/DropDown-2", module).add("Default", (args) => {
+//   return (
+//     <Dropdown
+//       menuWidth={args.number}
+//       onToggleChange={() => {
+//         action("change");
+//       }}
+//     >
+//       <h1>Test Dropdown</h1>
+//     </Dropdown>
+//   );
+// });
